511923823d104938817ea92161faf807145e0dd1
gogoringhye <145514996+gogoringhye@users.noreply.github.com>
2023-12-19 08:30:31 +0900

    Add files via upload

--- restart-main/사이트 연습/site ex/main.js
+++ restart-main/사이트 연습/site ex/main.js
@@ -1,8 +1,10 @@
-$('.cti-btn').click(function(e){
+$('.tit .btn').click(function(ev){
     ev.preventDefault();
-    //$('.cont_menu').toggle();
-    //$('.cont_menu').fadeToggle();
-    $('.cont_menu').slideToggle();
-    $(this).toggleClass('on');
+    //$('.nav').toggle();
+    //$('.nav').fadeToggle();
+    $('.nav').slideToggle();
+    $(this).toggleClass("on");
 });
+
+
 /* 왜 고글에 손을 올려도 on 버튼이 안뜨지 */
