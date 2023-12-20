b184661c01fecf848d29ca69094fdbd829689aa2
gogoringhye <145514996+gogoringhye@users.noreply.github.com>
2023-12-21 08:49:23 +0900

    Delete practice directory

--- practice/사이트 연습, header까지 완성/site ex/main.js
+++
@@ -1,8 +0,0 @@
-$('.cti-btn').click(function(e){
-    ev.preventDefault();
-    //$('.cont_menu').toggle();
-    //$('.cont_menu').fadeToggle();
-    $('.cont_menu').slideToggle();
-    $(this).toggleClass('on');
-});
-/* 왜 고글에 손을 올려도 on 버튼이 안뜨지 */
